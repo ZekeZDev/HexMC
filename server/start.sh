f19cea2a4ea12d6e08c29db44543cf743e268990
Ezekiel Smith <ezekielsmith@protonmail.com>
2022-07-02 14:52:08 +1000

    update

--- server/start.sh
+++ server/start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Update packwiz
-java -jar packwiz-installer-bootstrap.jar -g -s server https://raw.githubusercontent.com/ZekeSmith/HexMC/dev/pack/.minecraft/pack.toml
+java -jar packwiz-installer-bootstrap.jar -g -s server https://zekesmith.github.io/HexMC/pack/.minecraft/pack.toml
 
 # Run Server
 java -Xms512M -Xmx4096M -Dterminal.jline=false -Dterminal.ansi=true -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:InitiatingHeapOccupancyPercent=15 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -jar server.jar nogui
